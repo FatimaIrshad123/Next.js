3d7b9b0bf6d811d395019d2625c6a937d8a80c09
FatimaIrshad <bibimemoona2017@gmail.com>
2024-03-16 10:50:33 +0500

    Added signup page

--- my-app/app/api/user/route.ts
+++ my-app/app/api/user/route.ts
@@ -1,7 +1,17 @@
 
+import { NextRequest } from "next/server";
+
 export function GET(){
     return Response.json({
         email: 'fatima@gmail.com',
         name: 'fatima'
+    })
+}
+
+export async function POST(req: NextRequest) {
+    const body = await req.json()
+    console.log(body)
+    return Response.json({
+        message : "User Created"
     })
 }
---
+++ my-app/app/signup/page.tsx
@@ -0,0 +1,8 @@
+import { Signup } from "@/components/Signup"
+export default function SignupComponent(){
+    return (
+        <div>
+            <Signup/>
+        </div>
+    )
+}
