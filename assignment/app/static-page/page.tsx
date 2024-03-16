242e5b5bc16308751137b68e453bb034035b92db
FatimaIrshad <bibimemoona2017@gmail.com>
2024-03-16 22:17:47 +0500

    Added Static Page

---
+++ assignment/app/static-page/page.tsx
@@ -0,0 +1,19 @@
+
+export default function StaticPage(){
+    return (
+    <div className="p-5 mx-72">
+        <div className="justify-center">
+            <h1 className="font-bold text-3xl m-3">Welcome to Static Page</h1>
+        </div>
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut 
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                 esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
+                 in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+    </div>
+  )
+}
