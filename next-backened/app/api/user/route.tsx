fca43b24484f2658b32ac5cf2dff1e1c14c2d1ff
FatimaIrshad <bibimemoona2017@gmail.com>
2024-03-20 12:17:01 +0500

    Added database

--- next-backened/app/api/user/route.tsx
+++ next-backened/app/api/user/route.tsx
@@ -1,18 +1,26 @@
 import { NextResponse,NextRequest } from "next/server";
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
-export function GET(){
+export async function GET(){
+    const user = await prisma.user.findFirst({})
     return NextResponse.json({
-        email:"Fatima@gmail.com",
-        name:"Fatima"
+        user
     })
 }
 
 export async function POST(req: NextRequest){
     const body = await req.json()
+    await prisma.user.create({
+        data:{
+            email:body.email,
+            password:body.password
+        }
+    })
     // const header = req.headers.get("authorization")
     // query params
     // console.log(req.nextUrl.searchParams.get("name"))
     return NextResponse.json({
-       body
+        body
     })
 }
