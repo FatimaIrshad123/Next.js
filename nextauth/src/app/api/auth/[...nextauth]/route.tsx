b1751c370f6caeda410da3995b39afe04a51675f
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-03-30 12:07:18 +0500

    Added NextAuth practice

---
+++ nextauth/src/app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,26 @@
+import { NextApiRequest } from "next";
+import NextAuth from "next-auth/next";
+import { NextResponse } from "next/server";
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name:"Email",
+            credentials: {
+                username: {label:'email', type:'text',placeholder:'Email'},
+                password: {label:'password', type:'password',placeholder:'Password'}
+            },
+            async authorize(credentials:any){
+                console.log(credentials)
+                return {
+                    id: "user1",
+                    name:'Fatima',
+                    email:'abcd'
+                };
+            }
+        })
+    ]
+})
+export const GET = handler;
+export const POST = handler;
