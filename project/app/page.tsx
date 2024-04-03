51b064ab56541cf257f7e77b1a56bfb1282eeabe
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-04-03 11:51:27 +0500

    added featured Card component

--- project/app/page.tsx
+++ project/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
@@ -5,8 +6,8 @@ export default function Home() {
   return (
     <div>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/ [0.02]">
-        <h1 className="text-2xl text-center">Hello</h1>
         <HeroSection />
+        <FeaturedCourses/>
       </main>
     </div>  
   );
